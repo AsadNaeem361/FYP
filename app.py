bd2e6ef9749922868877b811ea869fff671b0083
Asadnaeem361 <asadnaeem361@gmail.com>
2023-05-01 23:05:43 +0100

    A bit of penultimate code cleaning and actual best model joblib file uplaoded

--- app.py
+++ app.py
@@ -71,15 +71,6 @@ def main():
 
     user_session = create_user_session()
 
-    # if user_session['logged_in'] is True:
-        # app.add_app("Best model predictor", myapp.page1)
-        # app.add_app("Build your own model", myapp.page2)
-                
-    #     # # The main app
-        # app.run()
-        
-
-
     # Authentication
     option = st.sidebar.selectbox('Login/Signup/Reset-Password', ['Login', 'Sign up', 'Reset Password'])
 
@@ -116,46 +107,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-    
-# https://firebase.google.com/docs/reference/rest/auth
-
-
-# def main():
-
-#     if user_session['logged_in'] is True:
-#         st.write("Authentication already established")
-
-#     # Authentication
-#     option = st.sidebar.selectbox('Login/Signup/Reset-Password', ['Login', 'Sign up', 'Reset Password'])
-
-#     # Sign up Block
-#     if option == 'Sign up':
-#         email = st.sidebar.text_input("Email")
-#         password = st.sidebar.text_input("Password", type="password")
-#         submit = st.sidebar.button('Create my account')
-#         if submit:
-#             signup(email, password)
-
-#     if option == "Reset Password":
-#         email = st.sidebar.text_input("Email")
-#         if st.sidebar.button("Reset Password"):
-#             resetPass(email)
-
-#     if option == "Login":
-#         user_session = create_user_session()
-#         email = st.sidebar.text_input("Email")
-#         password = st.sidebar.text_input("Password", type="password")
-#         if st.sidebar.button("Login"):     
-#             if login(email, password):
-#                 user_session['logged_in'] = True
-#                 app.add_app("Best model predictor", myapp.page1)
-#                 app.add_app("Build your own model", myapp.page2)
-                
-#                 # The main app
-#                 app.run()
-#     # uploaded_file = st.file_uploader("Upload Files",type=['csv'])
-#     # app.add_app("Best model predictor", myapp.page1)
-#     # app.add_app("Build your own model", myapp.page2)
-    
-#     # # The main app
-#     # app.run()
